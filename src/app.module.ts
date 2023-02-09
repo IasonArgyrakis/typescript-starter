3012d9375d63637bbca625fda53237d35a3e5395
Iason Argyrakis <jasonargirakis@gmail.com>
2023-02-09 20:44:57 +0200

    ORMs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 
+
 import { UsersService } from './users/users.service';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
@@ -8,9 +9,23 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { DepartmentsModule } from './departments/departments.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { Department } from './departments/entities/department.entity';
 
 @Module({
-  imports: [PrismaModule,UsersModule, AuthModule, DepartmentsModule],
+  imports: [
+      TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: 'username',
+      password: 'password',
+      database: 'docker-nest-postgres',
+      entities: [User,Department],
+      synchronize: true,
+    }),
+    PrismaModule,UsersModule, AuthModule, DepartmentsModule],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
