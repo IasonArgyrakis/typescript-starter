41e44a9429150508f5d2edeab6deb726bfef3419
Iason Argyrakis <jasonargirakis@gmail.com>
2023-02-09 16:28:53 +0200

    added db stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
+import { UsersService } from './users/users.service';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { DepartmentsModule } from './departments/departments.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule, DepartmentsModule],
   controllers: [AppController, UsersController],
-  providers: [AppService],
+  providers: [AppService,UsersService],
 })
 export class AppModule {}
